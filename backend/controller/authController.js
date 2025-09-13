13f6be44ea84455285ed5e83c01385ec591e1da4
shivsingh78 <shivsinghbaghel95@gmail.com>
2025-09-14 01:59:56 +0530

    feat: Update cookie settings for enhanced security in authentication methods

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -37,8 +37,8 @@ export const registration = async (req, res) => {
           let token = await genToken(user._id)
           res.cookie("token", token, {
                httpOnly: true,
-               secure: false,
-               sameSite: "lax",
+               secure: true,
+               sameSite: "none",
                maxAge: 7 * 24 * 60 * 60 * 1000
 
           })
@@ -75,8 +75,8 @@ export const login = async (req, res) => {
           let token = await genToken(user._id)
           res.cookie("token", token, {
                httpOnly: true,
-               secure: false,
-               sameSite: "lax",
+               secure: true,
+               sameSite: "none",
                maxAge: 7 * 24 * 60 * 60 * 1000
 
           })
@@ -116,8 +116,8 @@ export const googleLogin = async (req, res) => {
           let token = await genToken(user._id)
           res.cookie("token", token, {
                httpOnly: true,
-               secure: false,
-               sameSite: "Strict",
+               secure: true,
+               sameSite: "none",
                maxAge: 7 * 24 * 60 * 1000
 
           })
@@ -142,8 +142,8 @@ export const adminLogin = async (req, res) => {
                let token = await genToken1(email)
                res.cookie("token", token, {
                     httpOnly: true,
-                    secure: false,
-                    sameSite: "Strict",
+                    secure: true,
+                    sameSite: "none",
                     maxAge: 1 * 24 * 60 * 60 * 1000
                })
                 console.log("login attempt:", email, password);
