ef251983ed19602bd4a689e968f2f3d363c65775
shiv singh baghel <shivsinghbaghel95@gmail.com>
2025-09-14 02:15:17 +0530

    Update index.js change frontend URL

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ let app=express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-     origin:["http://localhost:5173" , "http://localhost:5174",],
+     origin:["https://e-store-frontend-4cvi.onrender.com" , "http://localhost:5174",],
      credentials:true
 }))
 
