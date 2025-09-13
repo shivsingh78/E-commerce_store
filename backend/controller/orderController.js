162c7a26144159f4793d7c32396a251402d58ff6
shivsingh78 <shivsinghbaghel95@gmail.com>
2025-09-13 08:17:12 +0530

    adding razorpay

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -1,7 +1,15 @@
 import Order from "../model/orderModel.js";
 import User from '../model/userModel.js'
+import razorpay from 'razorpay'
 
+//razorpay payment integration
+const currency = 'inr'
+const razorpayInstance = new razorpay({
 
+key_id: process.env.RAZORPAY_KEY_ID,
+key_secret:process.env.RAZORPAY_KEY_SECRET,
+
+})
 export const placeOrder = async (req,res) => {
      try {
           const {items , amount , address} = req.body;
@@ -37,6 +45,48 @@ export const placeOrder = async (req,res) => {
      }
      
 }
+//for payment 
+export const placeOrderRazorpay = async (req,res) => {
+     try {
+          const {items , amount , address} = req.body;
+          const userId = req.userId;
+          const orderData = {
+               items
+               ,amount,
+               userId,
+               address,
+               paymentMethod:'Razorpay',
+               payment:false,
+               date:Date.now()
+          }
+          const newOrder = new Order(orderData)
+          await newOrder.save()
+
+          const options = {
+               amount:amount*100,
+               currency:currency.toUpperCase(),
+               receipt: newOrder._id.toString()
+
+          }
+          await razorpayInstance.orders.create(options,(error,order)=>{
+               if(error) {
+                    console.log(error);
+                    return res.status(500).json(error)
+                    
+               }
+               res.status(200).json(order)
+          })
+
+
+     } catch (error) {
+          console.log(error);
+          res.status(500).json({message:error.message})
+          
+          
+     }
+     
+}
+
 
 // for user
 
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import isAuth from '../middleware/isAuth.js'
-import { allOrders, placeOrder, updateStatus, userOrders } from '../controller/orderController.js';
+import { allOrders, placeOrder, placeOrderRazorpay, updateStatus, userOrders } from '../controller/orderController.js';
 import adminAuth from '../middleware/adminAuth.js'
 
 const orderRoutes = express.Router();
 
 //for user 
 orderRoutes.post("/placeorder",isAuth,placeOrder)
+orderRoutes.post("/razorpay",isAuth,placeOrderRazorpay)
 orderRoutes.post("/userorder",isAuth,userOrders)
 
 //for admin
